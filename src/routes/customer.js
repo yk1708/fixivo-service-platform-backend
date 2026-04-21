54ceb9b41cca981f68c3964b5bd3e27365cb65a9
Yash Khude <khudeyash2021@gmail.com>
2026-04-21 15:24:49 +0530

    Remove authentication middleware from verified providers route

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const customerController = require("../controllers/customerController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/verified-providers", authMiddleware.verifyToken, customerController.getVerifiedProviders);
+router.get("/verified-providers", customerController.getVerifiedProviders);
 router.get("/emergencies/:emergencyId/status", authMiddleware.verifyToken, customerController.getEmergencyStatus);
 
 module.exports = router;
